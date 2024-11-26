d57537e8ddee1d57516141ea463da1641f14286e
paul <paul@dangersalad.com>
2024-11-26 13:47:06 -0700

    add more stuff

--- init.el
+++ init.el
@@ -249,6 +249,120 @@
          ("C-c C-_ C-c" . evilnc-copy-and-comment-lines)
          ("C-c C-_ C-p" . evilnc-comment-or-uncomment-paragraphs)))
 
+;; search
+(use-package consult
+  :straight t
+  :after projectile
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-s" . consult-line)
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history)
+         :map projectile-command-map               ;; orig. previous-matching-history-element
+         ("s s" . consult-ripgrep)
+         ("f"   . consult-find))
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  (setq consult-project-function (lambda (_) (projectile-project-root)))
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+)
+
 ;; minibuffer completion stuff
 ;; Enable vertico
 (use-package vertico
@@ -291,14 +405,6 @@
         '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
 
-;; Optionally use the `orderless' completion style.
-;; (use-package orderless
-;;   :straight t
-;;   :custom
-;;   (completion-styles '(orderless partial-completion basic))
-;;   (completion-category-defaults nil)
-;;   (completion-category-overrides nil))
-
 (use-package fussy
   :straight t
   :ensure t
@@ -315,22 +421,23 @@
   (setq corfu-auto t
         corfu-quit-no-match 'separator))
 
-;; A few more useful configurations...
 (use-package emacs
   :custom
   (tab-always-indent 'complete)
-  ;; (text-mode-ispell-word-completion nil)
-
-  ;; Hide commands in M-x which do not apply to the current mode.  Corfu
-  ;; commands are hidden, since they are not used via M-x. This setting is
-  ;; useful beyond Corfu.
   (read-extended-command-predicate #'command-completion-default-include-p))
+
+(use-package treesit-auto
+  :straight t
+  :custom ((treesit-auto-install 'prompt))
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
 
 ;; LSP stuff
 (use-package yasnippet
   :straight t
   :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode))
+  :hook (go-ts-mode . yas-minor-mode))
 
 (use-package all-the-icons
   :straight t)
@@ -364,10 +471,9 @@
            (lsp-go-use-placeholders nil "Don't insert snippets."))
   :defines (lsp-gopls-hover-kind lsp-gopls-env ds/setup-lsp-save-hooks)
   :commands (lsp lsp-deferred lsp-register-custom-settings)
-  :hook ((go-mode . lsp-deferred)
-         (typescript-mode . lsp-deferred)
-         (svelte-mode . lsp-deferred)
-         (js-mode . lsp-deferred)
+  :hook ((go-ts-mode . lsp-deferred)
+         (typescript-ts-mode . lsp-deferred)
+         (js-ts-mode . lsp-deferred)
          (lsp-completion-mode . ds/lsp-mode-setup-completion))
   :init
   (setq lsp-keymap-prefix "C-c l")
@@ -388,9 +494,7 @@
 (use-package lsp-ui
   :straight t
   :commands lsp-ui-mode
-  :after (flycheck lsp-mode)
-  :bind (:map flycheck-mode-map
-              ("C-c ! l" . lsp-ui-flycheck-list))
+  :after (lsp-mode)
   :defines (lsp-ui-mode-map)
   :hook ((lsp-mode-hook . lsp-ui-mode))
   :custom ((lsp-ui-doc-use-webkit nil "Use webkit widget for LSP docs")
@@ -491,10 +595,8 @@ Only if there are no other windows in the frame, and if the buffer is in frames-
 ;; golang
 (use-package go-mode
   :straight t
-  :hook ((go-mode . ds/setup-lsp-save-hooks))
-  :mode ("\\go.mod\\'" . go-dot-mod-mode))
-
-(use-package go-template-mode)
+  :hook ((go-ts-mode . ds/setup-lsp-save-hooks))
+  :mode (("\\go.mod\\'" . go-dot-mod-mode)))
 
 ;; javascript
 (use-package typescript-mode
